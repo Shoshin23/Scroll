10e450d5e1041678ca79192ea730237378a6e8b4
Karthik Kannan <karthikk.314@gmail.com>
2013-04-04 01:50:43 +0530

    Update p1.rb

--- p1.rb
+++ p1.rb
@@ -1,24 +1,25 @@
-start_time = Time.now 
+start = Time.now 
 
 def d(n) #method to find the sum of divisors.
   sum = 1
   2.upto(Math.sqrt(n)) do |i| # upper bound for the sieve
     sum += i + (n / i) if (n % i == 0) # factorise and add.
   end
-  return sum #return the sum
+  return sum #returns the sum
 end
 
 sum = 0
 final = [] # array to store the final value
-1.upto(100000) do |i| 
+
+1.upto(100000) do |i| # the counter
   num_divisors = d(i) 
   if d(num_divisors) == i && i != num_divisors #implemented the main condition here
-    sum += i
+    sum += i 
     final << i #add number to the array.
   end
 end
 
-puts sum # output the sum.
+puts sum 
 
 end_time = Time.now 
 puts "Total Time: " + (end_time - start_time).to_s + " seconds." 
